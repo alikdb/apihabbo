556c5acc566ac844d51f9e9fca8c44d3c59bf0ea
Ali <44875945+alikdb@users.noreply.github.com>
2022-10-31 00:31:19 +0300

    Update BadgesName.php

--- app/Http/Controllers/API/BadgesName.php
+++ app/Http/Controllers/API/BadgesName.php
@@ -11,9 +11,9 @@ class BadgesName extends Controller
 {
     public $hotels = array('es', 'fr', 'com.br', 'it', 'com.tr', 'de', 'nl', 'fi', 'com');
     public function scanBadgeName() {
-
-        $this->getBadges('com.tr');
-
+        foreach($this->hotels as $hotel) {
+            $this->getBadges($hotel);
+        }
     }
     private function getBadges($hotel) {
         $url = "https://habbo." . $hotel . "/gamedata/external_flash_texts/0";
@@ -25,11 +25,13 @@ class BadgesName extends Controller
         foreach ($Statusraw2[1] as $key => $value) {
             $badgeName[trim($value)] = str_replace("\"", "´´", str_replace("'", "`", $Statusraw2[2][$key]));
         }
+        $count = 0;
         foreach ($badgeName as $code => $name) {
-
+                
             $bCount = BadgesNameModel::where(['code' => $code, 'hotel' => $hotel])->count();
 
             if ($bCount == 0) {
+                $count++;
                 BadgesNameModel::create([
                    'code' => $code,
                    'name' => $badgeName[$code],
@@ -45,5 +47,6 @@ class BadgesName extends Controller
             }
 
         }
+        echo $hotel . ' oteline '. $count .' rozet eklendi';
     }
 }
