d4adec5ac346ab6e7f0e68f79be9427881fb1b0c
alikdb <hello@alisen.me>
2022-11-05 23:28:32 +0300

    furnis hotel added fix

--- app/Models/Furnis.php
+++ app/Models/Furnis.php
@@ -9,5 +9,5 @@ class Furnis extends Model
 {
     use HasFactory;
     protected $table = 'furnis';
-    protected $fillable = ['hotel_id', 'name', 'code', 'revision', 'description', 'category'];
+    protected $fillable = ['hotel_id', 'name', 'code', 'revision', 'description', 'hotel', 'category'];
 }
